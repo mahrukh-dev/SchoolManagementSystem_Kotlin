a96b9f77cb56b435f0c1dcef72c0bb4669824767
MahRukh <mahrukhdev@gmail.com>
2023-07-25 15:06:20 +0500

    course class

--- src/main/kotlin/Classes/Course.kt
+++ src/main/kotlin/Classes/Course.kt
@@ -1,4 +1,29 @@
 package Classes
 
-class Course {
+class Course(id:Int, cname: String, cTeacher: Teacher, max:  Int, std: MutableList<Student>) {
+
+    private var _courseId: Int = id
+    var courseId: Int
+        get() {return _courseId}
+        set(value) {_courseId = value}
+
+    private var _courseName: String = cname
+    var courseName: String
+        get() {return _courseName}
+        set(value) {_courseName = value}
+
+    private var _teacher: Teacher = cTeacher
+    var teacher: Teacher
+        get() {return _teacher}
+        set(value) {_teacher = value}
+
+    private var _maxCapacity: Int = max
+    var maxCapacity: Int
+        get() {return _maxCapacity}
+        set(value) {_maxCapacity = value}
+
+    private var _enrolledStudents: MutableList<Student> = std
+    var enrolledStudents: MutableList<Student>
+        get() {return _enrolledStudents}
+        set(value) {_enrolledStudents = value}
 }
--- src/main/kotlin/Classes/Teacher.kt
+++ src/main/kotlin/Classes/Teacher.kt
@@ -1,4 +1,18 @@
 package Classes
 
-class Teacher {
+class Teacher(name: String, age: Int, id: Int, subj: String): Person(name, age) {
+    private var _teacherId: Int = id
+    var teacherId: Int
+        get() {return _teacherId}
+        set(value) {_teacherId = value}
+
+    private var _subject: String = subj
+    var subject: String
+        get() {return _subject}
+        set(value) {_subject = value}
+
+    init{
+        subject = subj
+        teacherId = id
+    }
 }
