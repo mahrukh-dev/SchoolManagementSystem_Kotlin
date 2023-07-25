e46a6ca6b749a565b82dc4fbf7e14683d80edea4
MahRukh <mahrukhdev@gmail.com>
2023-07-25 13:33:36 +0500

    Student Class

--- src/main/kotlin/Classes/Person.kt
+++ src/main/kotlin/Classes/Person.kt
@@ -1,15 +1,20 @@
 package Classes
 
-class Person {
-    private var _name: String = ""
+open class Person(namee: String, agee: Int) {
+    private var _name: String = namee
     var name: String
         get() {return _name}
         set(value) {_name = value}
 
 
-    private var _age:Int=0 //backing field hold actual property data
+    private var _age:Int = agee //backing field hold actual property data
     var age:Int //backing property is exposed
         get() {return _age}
         set(value) {_age = value}
+
+    init{
+        this.age = _age
+        this.name = name
+    }
 
 }
---
+++ src/main/kotlin/Classes/Student.kt
@@ -0,0 +1,24 @@
+package Classes
+
+class Student(name: String, age: Int, id: Int, gradee: Int, classList: MutableList<String>): Person(name, age) {
+    private var _studentId: Int = id
+    var studentId: Int
+        get() {return _studentId}
+        set(value) {_studentId = value}
+
+    private var _grade: Int = gradee
+    var grade: Int
+        get() {return _grade}
+        set(value) {_grade = value}
+
+    private var _enrolledClass = classList
+    var enrolledClass : MutableList<String>
+        get() {return _enrolledClass}
+        set(value) {_enrolledClass = value}
+
+    init{
+        grade = gradee
+        studentId = id
+        enrolledClass = classList
+    }
+}
