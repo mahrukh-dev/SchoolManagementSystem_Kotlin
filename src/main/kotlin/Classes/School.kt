024f3c92fd76bbc1c94d9b19d1c49b18cd8e7152
MahRukh <mahrukhdev@gmail.com>
2023-07-25 16:08:41 +0500

    School Class

--- src/main/kotlin/Classes/School.kt
+++ src/main/kotlin/Classes/School.kt
@@ -1,7 +1,9 @@
 package Classes
 
 class School {
+
     val students: MutableList<Student> = mutableListOf()
     val courses: MutableList<Course> = mutableListOf()
     val teachers: MutableList<Teacher> = mutableListOf()
+
 }
